583bd6ec328ed68a46a8013d6695a44742310aa5
Hunter <msilas007@gmail.com>
2026-07-29 04:06:11 +0100

    Modified scripts for automation

--- scripts/webapp-deployment.sh
+++ scripts/webapp-deployment.sh
@@ -29,7 +29,7 @@ echo ""
 
 cd infra/envs/prod
 
-terraform apply -target=module.ecs -target=module.dns_acm.aws_acm_certificate.this -target=module.alb -var="image_uri=${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/opsshield:latest" -auto-approve
+terraform apply -target=module.ecs -var="image_uri=${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/opsshield:latest" -auto-approve
 
 CLUSTER=$(terraform output -raw ecs_cluster_name)
 TASK_DEF=$(terraform output -raw ecs_task_definition_arn)
